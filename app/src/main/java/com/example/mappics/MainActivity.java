5381b788ab3465b14873668610fd5eff9d67d222
Michael Douglas Dowlin <mdowlin@uwyo.edu>
2019-02-22 16:03:16 -0700

    Got a good start
    Can return pictures and start map

---
+++ app/src/main/java/com/example/mappics/MainActivity.java
@@ -0,0 +1,182 @@
+package com.example.mappics;
+
+import android.Manifest;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+
+
+import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.location.Location;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.location.SettingsClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.LocationSettingsStatusCodes;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+public class MainActivity extends FragmentActivity implements OnMapReadyCallback{
+
+    public static final int REQUEST_ACCESS_startLocationUpdates = 0;
+    public static final int REQUEST_ACCESS_onConnected = 1;
+    /**
+     * Constant used in the location settings dialog.
+     */
+    private static final int REQUEST_CHECK_SETTINGS = 0x1;
+
+
+    Location mLastLocation;
+    LocationRequest mLocationRequest;
+    ImageView iv;
+
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationClient;
+    private SettingsClient mSettingsClient;
+    private LocationSettingsRequest mLocationSettingsRequest;
+
+    String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        iv = findViewById(R.id.imageView);
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
+        mSettingsClient = LocationServices.getSettingsClient(this);
+
+       //MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        //mapFragment.getMapAsync(this);
+
+
+        FloatingActionButton fab = findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, 0);
+            }
+        });
+
+        buildLocationSettingsRequest();
+        getPermission();
+
+        //If reached here, will have location permissions
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        mMap.addMarker(new MarkerOptions()
+                .position(new LatLng(0, 0))
+                .title("Marker"));
+    }
+
+    /**
+     * Uses a {@link com.google.android.gms.location.LocationSettingsRequest.Builder} to build
+     * a {@link com.google.android.gms.location.LocationSettingsRequest} that is used for checking
+     * if a device has the needed location settings.
+     */
+    private void buildLocationSettingsRequest() {
+        LocationSettingsRequest.Builder builder = new LocationSettingsRequest.Builder();
+        builder.addLocationRequest(mLocationRequest);
+        mLocationSettingsRequest = builder.build();
+    }
+
+    protected void getPermission() {
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED)
+        {
+            Log.v(TAG, "asking for permissions");
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                    MainActivity.REQUEST_ACCESS_startLocationUpdates);
+        }
+    }
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        Log.v(TAG, "onRequest result called.");
+        boolean fine = false;
+
+        //received result for GPS access
+        for (int i = 0; i < grantResults.length; i++) {
+            if ((permissions[i].compareTo(Manifest.permission.ACCESS_FINE_LOCATION) == 0) &&
+                    (grantResults[i] == PackageManager.PERMISSION_GRANTED))
+                fine = true;
+        }
+        Log.v(TAG, "Received response for gps permission request.");
+        // If request is cancelled, the result arrays are empty.
+        if (fine) {
+            // permission was granted
+            Log.v(TAG, permissions[0] + " permission has now been granted. Showing preview.");
+            Toast.makeText(this, "GPS access granted",
+                    Toast.LENGTH_SHORT).show();
+
+
+        } else {
+            // permission denied,    Disable this feature or close the app.
+            Log.v(TAG, "GPS permission was NOT granted.");
+            Toast.makeText(this, "GPS access NOT granted", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        //the picture is stored in the intent in the data key.
+        //get the picture and show it in an the imagview.
+        //Note the picture is not stored on the filesystem, so this is the only "copy" of the picture.
+        Bundle extras = data.getExtras();
+        Log.i(TAG, "Works well to here");
+        if (extras != null) {
+            //if you know for a fact there will be a bundle, you can use  data.getExtras().get("Data");  but we don't know.
+            Bitmap bp = (Bitmap) extras.get("data");
+            iv.setImageBitmap(bp);
+            iv.invalidate();
+        } else {
+            Toast.makeText(this, "No picture was returned", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
